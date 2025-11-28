9f1f9fa7056efb44a0ccda75cda4183744ba3751
KrokodilGena07 <pasha.fenev@gmail.com>
2025-11-29 01:09:40 +0300

    some changes

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const token = JSON.parse(localStorage.getItem('Token') || 'null');
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://chat-app-server-r94h.onrender.com/api',
     withCredentials: true,
     headers: token ? {
         Authorization: `Bearer ${token}`
--- src/api/wss.ts
+++ src/api/wss.ts
@@ -2,4 +2,4 @@ import {useUser} from '@/store/useUser';
 
 const accessToken = useUser.getState().accessToken;
 const token = encodeURIComponent(accessToken);
-export default new WebSocket(`ws://localhost:8000?token=${token}`, ['soap', 'wamp']);
+export default new WebSocket(`ws://chat-app-server-r94h.onrender.com?token=${token}`, ['soap', 'wamp']);
