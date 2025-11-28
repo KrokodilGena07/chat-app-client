3d3edf389ff714c87680a452d21375759c624ba2
KrokodilGena07 <pasha.fenev@gmail.com>
2025-11-29 02:34:28 +0300

    del row

--- src/api/wss.ts
+++ src/api/wss.ts
@@ -2,4 +2,4 @@ import {useUser} from '@/store/useUser';
 
 const accessToken = useUser.getState().accessToken;
 const token = encodeURIComponent(accessToken);
-export default new WebSocket(`wss://chat-app-server-r94h.onrender.com:8000?token=${token}`, ['soap', 'wamp']);
+export default new WebSocket(`wss://chat-app-server-r94h.onrender.com?token=${token}`, ['soap', 'wamp']);
