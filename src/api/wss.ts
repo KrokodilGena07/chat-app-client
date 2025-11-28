238209af7df2be7e4e876e184b88d1c74fc4bcb2
KrokodilGena07 <pasha.fenev@gmail.com>
2025-11-29 01:19:21 +0300

    some changes

--- src/api/wss.ts
+++ src/api/wss.ts
@@ -2,4 +2,4 @@ import {useUser} from '@/store/useUser';
 
 const accessToken = useUser.getState().accessToken;
 const token = encodeURIComponent(accessToken);
-export default new WebSocket(`ws://chat-app-server-r94h.onrender.com?token=${token}`, ['soap', 'wamp']);
+export default new WebSocket(`wss://chat-app-server-r94h.onrender.com?token=${token}`, ['soap', 'wamp']);
